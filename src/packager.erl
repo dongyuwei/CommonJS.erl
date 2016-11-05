6051ac9a7753d050a0992e0385c8b844b7429b79
Dong Yuwei <newdongyuwei@gmail.com>
2016-11-05 21:27:17 +0800

    spawn process per js-entry-file

--- src/packager.erl
+++ src/packager.erl
@@ -1,18 +1,34 @@
 -module (packager).
--export ([bundle_single_js/1, bundle/2]).
+-export ([bundle_js_in_dir/1, bundle/1]).
 
-bundle(Input_dir, Output_dir) ->
+bundle_js_in_dir(Input_dir) ->
     Js_files = filelib:wildcard(filename:join(Input_dir, filename:join("**", "*.js"))),
-    lists:foreach(fun(Js_file) -> bundle_single_js(Js_file) end, Js_files).
+    Js_files2 = lists:filter(
+        fun(Js_file) -> 
+            lists:prefix("_", filename:basename(Js_file)) =:= false 
+        end, 
+    Js_files),
 
-bundle_single_js(Js_entry_file) ->
-    bundle_single_js2(Js_entry_file, Js_entry_file).
+    lists:foreach(
+        fun(Js_file) ->
+            spawn(?MODULE, bundle, [Js_file])
+        end, 
+    Js_files2) .
 
 
-bundle_single_js2(Js_entry_file, Required_name) ->
-    io:format("load ~p ~n", [Required_name]),
+bundle(Js_entry_file) ->
+    bundle2(Js_entry_file, Js_entry_file),
+    io:format("~p ~n", [get()]),
+    [_, _, _, {compile, [_, _, {source, Source}]}, _, _] = ?MODULE:module_info(),
+    {ok, Require_fun} = file:read_file(filename:join(filename:dirname(Source), "require.js")),
+    io:format("~p ~n", [Require_fun]).
+    % "(function(){\n" + Require_fun + \nrequire.sourceCache = {};\n" + source "\n})();",
+    % file:write_file(Js_entry_file + "-packed.js", Content).
+
+bundle2(Js_entry_file, Required_module_name) ->
+    io:format("processing ~p ~n", [Required_module_name]),
     {ok, Content} = file:read_file(Js_entry_file),
-    put(Required_name, Content), 
+    put(Required_module_name, Content), 
     Require_regexp = "(require\\((['|\"])(.*?)\\g2\\);?)",
     case re:run(Content, Require_regexp, [global,{capture,[3],list}]) of
     {match, Matched} ->
@@ -20,11 +36,10 @@ bundle_single_js2(Js_entry_file, Required_name) ->
             fun(Item) -> 
                 [Required_module] = Item,
                 Required_js_path = filename:join(filename:dirname(Js_entry_file), Required_module),
-                bundle_single_js2(Required_js_path, Required_module)
+                bundle2(Required_js_path, Required_module)
             end
         , Matched);
     nomatch ->
         nomatch
-    end,
-    % https://www.erlang.org/course/advanced#dict
-    get().
+    end .
+
