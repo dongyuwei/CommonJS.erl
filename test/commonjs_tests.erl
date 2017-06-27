78a78b741b79d40e4d0b301d9a1271100f1ea61d
Dong Yuwei <newdongyuwei@gmail.com>
2017-06-27 17:03:09 +0800

    use indent 4

--- test/commonjs_tests.erl
+++ test/commonjs_tests.erl
@@ -2,21 +2,21 @@
 -include_lib("eunit/include/eunit.hrl").
 
 bundle_single_js_test() ->
-  os:cmd("rm /tmp/commonjs_build/entry_bundled.js"),
+    os:cmd("rm /tmp/commonjs_build/entry_bundled.js"),
 
-  Bundled_content = commonjs:bundle_single_js("./test/commonjs/entry.js", "/tmp/commonjs_build/entry_bundled.js"),
-  {ok, Bundled_file_content} = file:read_file("/tmp/commonjs_build/entry_bundled.js"),
-  ?assertEqual(Bundled_content, Bundled_file_content),
+    Bundled_content = commonjs:bundle_single_js("./test/commonjs/entry.js", "/tmp/commonjs_build/entry_bundled.js"),
+    {ok, Bundled_file_content} = file:read_file("/tmp/commonjs_build/entry_bundled.js"),
+    ?assertEqual(Bundled_content, Bundled_file_content),
 
-  {ok, Content} = file:read_file("test/commonjs/entry.js.txt"),
-  ?assertEqual(Content, list_to_binary(os:cmd("node /tmp/commonjs_build/entry_bundled.js"))).
+    {ok, Content} = file:read_file("test/commonjs/entry.js.txt"),
+    ?assertEqual(Content, list_to_binary(os:cmd("node /tmp/commonjs_build/entry_bundled.js"))).
 
 
 bundle_js_in_dir_test() ->
-  os:cmd("rm -rf /tmp/commonjs_build"),
-  commonjs:bundle_js_in_dir("./test", "/tmp/commonjs_build", false),
-  Bundled_count = string:strip(string:strip(os:cmd("ls /tmp/commonjs_build/commonjs/*.js | wc -l")), both, $\n),
-  ?assertEqual(Bundled_count, "2").
+    os:cmd("rm -rf /tmp/commonjs_build"),
+    commonjs:bundle_js_in_dir("./test", "/tmp/commonjs_build", false),
+    Bundled_count = string:strip(string:strip(os:cmd("ls /tmp/commonjs_build/commonjs/*.js | wc -l")), both, $\n),
+    ?assertEqual(Bundled_count, "2").
 
 rebuild_changed_js_test() ->
     os:cmd("rm /tmp/commonjs_build/commonjs/entry2.js"),
